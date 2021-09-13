417b5e09084a92ab12dc2a0ab11b56716128aeda
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2021-09-13 13:27:32 +0000

    finished versions

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-import copy
+# 4-new_in_list.py
+
+
 def new_in_list(my_list, idx, element):
+    """Replace an element in a copied list at a specific position."""
     if idx < 0 or idx > (len(my_list) - 1):
-        return my_list
-    else:
-        copyList = copy.deepcopy(my_list)
-        copyList[idx] = element
-        return copyList
+        return (my_list)
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+# 5-no_c.py
+
+
 def no_c(my_string):
-    str1 = ""
-    size = len(my_string)
-    for x in range(0, size):
-        if(my_string[x] != "C" and my_string[x] != "c"):
-            str1 = str1 + my_string[x]
-    return(str1)
+    """Remove all characters c and C from a string."""
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
