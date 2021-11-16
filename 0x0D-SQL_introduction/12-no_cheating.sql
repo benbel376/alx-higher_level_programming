ba492a34e03d56a3d6042f5947e9e9dafd83719e
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2021-11-16 18:52:48 +0000

    adding task 12

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,6 +1,4 @@
 --a script that updates the score of Bob to 10 in the table second_table
 
-SELECT score, name FROM second_table
-ORDER BY score DESC;
 UPDATE second_table SET score = 10 WHERE name = 'Bob';
 
