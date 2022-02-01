30d052286afafb9f794a9bbe508ac82df90697ff
biniyam belayneh <biniyambelayneh376@gmail.com>
2022-02-01 11:11:37 -0800

    commit 2 task 3 editing typo in the url

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,5 +2,5 @@
 const request = require('request');
 let url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+    console.log(error || JSON.parse(bod:y).title);
 });
